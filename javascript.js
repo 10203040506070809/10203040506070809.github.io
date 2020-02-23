d0968001255506f9798063f527cf23d8fb089bb7
Jack Fletcher <Jackfletch4@gmail.com>
2020-02-23 23:37:13 +0000

    Updated projects page

---
+++ javascript.js
@@ -0,0 +1,11 @@
+< script type = "text/javascript" >
+    function openMenuById(id) {
+        var element = document.getElementById(id);
+
+        if (element.style.display == "none") {
+            element.style = "display:block";
+        } else {
+            element.style = "display:none";
+        }
+    } <
+    /script>
